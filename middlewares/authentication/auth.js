993d7778f8102bd950d39002b9bf5b35470478ae
chimezie collins obinwa <collinschimezieobinwa@gmail.com>
2024-07-25 20:12:21 +0200

    Implemented JWT  strategy for admin & user authentication

--- middlewares/authentication/auth.js
+++ middlewares/authentication/auth.js
@@ -3,8 +3,56 @@ const LocalStrategy = require('passport-local').Strategy;
 const JWTstrategy = require('passport-jwt').Strategy;
 const ExtractJWT = require('passport-jwt').ExtractJwt;
 const UserModel = require('../../models/users');
+const AdminModel = require('../../models/admins');
 
 
+// passport.use(
+//     new JWTstrategy(
+//         {
+//             secretOrKey: process.env.JWT_SECRET,
+//             jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken()
+//         },
+//         async (token, done) => {
+//             try {
+//                 // Check if the token is expired
+//                 if (Date.now() >= token.exp * 1000) {
+//                     return done(null, false, { message: 'Token expired' });
+//                 }
+
+//                 // Token is valid, return the user
+//                 return done(null, token.user);
+//             } catch (error) {
+//                 done(error);
+//             }
+//         }
+//     )
+// );
+
+
+
+// Admin JWT strategy
+passport.use(
+    new JWTstrategy(
+        {
+            secretOrKey: process.env.ADMIN_JWT_SECRET,
+            jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken()
+        },
+        async (token, done) => {
+            try {
+                const admin = await AdminModel.findById(token.admin._id);
+                if (!admin) {
+                    return done(null, false, { message: 'Admin not found' });
+                }
+                return done(null, admin);
+            } catch (error) {
+                done(error);
+            }
+        }
+    )
+);
+
+
+// User JWT strategy
 passport.use(
     new JWTstrategy(
         {
@@ -13,13 +61,11 @@ passport.use(
         },
         async (token, done) => {
             try {
-                // Check if the token is expired
-                if (Date.now() >= token.exp * 1000) {
-                    return done(null, false, { message: 'Token expired' });
+                const user = await UserModel.findById(token.user._id);
+                if (!user) {
+                    return done(null, false, { message: 'User not found' });
                 }
-                
-                // Token is valid, return the user
-                return done(null, token.user);
+                return done(null, user);
             } catch (error) {
                 done(error);
             }
@@ -29,37 +75,9 @@ passport.use(
 
 
 
+// Admin Sign Up
 passport.use(
-    'login',
-    new LocalStrategy(
-        {
-            usernameField: 'email',
-            passwordField: 'password'
-        },
-        async (email, password, done) => {
-            try {
-                console.log("IN PASSPORT LOGIN")
-
-                const user = await UserModel.findOne({ email });
-                if (!user) {
-                    return done(null, false, { message: 'User not found' });
-                }
-                const validate = await user.isValidPassword(password);
-                if (!validate) {
-                    return done(null, false, { message: 'Wrong Password' });
-                }
-                return done(null, user, { message: 'Logged in Successfully' });
-            } catch (error) {
-                return done(error);
-            }
-        }
-    )
-);
-
-
-
-passport.use(
-    'signup',
+    'admin-signup',
     new LocalStrategy(
         {
 
@@ -69,13 +87,96 @@ passport.use(
 
         },
         async (req, email, password, done) => {
-            const { first_name, last_name } = req.body;
+            const { fullname } = req.body;
             try {
-                console.log("IN PASSPORT SIGNUP")
-                const user = await UserModel.create({ first_name, last_name, email, password });
+                console.log("IN PASSPORT ADMIN SIGNUP")
+                const user = await AdminModel.create({ fullname, email, password });
                 return done(null, user);
             } catch (error) {
                 done(error);
+            }
+        }
+    )
+);
+
+
+// Admin login strategy
+passport.use(
+    'admin-login',
+    new LocalStrategy(
+        {
+            usernameField: 'email',
+            passwordField: 'password'
+        },
+        async (email, password, done) => {
+            try {
+                const admin = await AdminModel.findOne({ email });
+                if (!admin) {
+                    return done(null, false, { message: 'Admin not found' });
+                }
+                const validate = await admin.isValidPassword(password);
+                if (!validate) {
+                    return done(null, false, { message: 'Wrong Password' });
+                }
+                return done(null, admin, { message: 'Admin logged in Successfully' });
+            } catch (error) {
+                return done(error);
+            }
+        }
+    )
+);
+
+
+
+
+
+// User Sign Up
+passport.use(
+    'user-signup',
+    new LocalStrategy(
+        {
+
+            usernameField: 'email',
+            passwordField: 'password',
+            passReqToCallback: true
+
+        },
+        async (req, email, password, done) => {
+            const { username } = req.body;
+            try {
+                console.log("IN PASSPORT USER SIGNUP")
+                const user = await UserModel.create({ username, email, password });
+                return done(null, user);
+            } catch (error) {
+                done(error);
+            }
+        }
+    )
+);
+
+
+
+// User login strategy
+passport.use(
+    'user-login',
+    new LocalStrategy(
+        {
+            usernameField: 'email',
+            passwordField: 'password'
+        },
+        async (email, password, done) => {
+            try {
+                const user = await UserModel.findOne({ email });
+                if (!user) {
+                    return done(null, false, { message: 'User not found' });
+                }
+                const validate = await user.isValidPassword(password);
+                if (!validate) {
+                    return done(null, false, { message: 'Wrong Password' });
+                }
+                return done(null, user, { message: 'User logged in Successfully' });
+            } catch (error) {
+                return done(error);
             }
         }
     )
--- routes/auth.js
+++ routes/auth.js
@@ -4,19 +4,50 @@ const passport = require('passport');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-
-authRouter.post('/signup', passport.authenticate('signup', { session: false }), async (req, res, next) => {
+// Admin Sign Up
+authRouter.post('/admin/signup', passport.authenticate('admin-signup', { session: false }), async (req, res, next) => {
     res.json({
-        message: 'Signup successful',
+        message: 'Admin Signup successful',
+        user: req.user
+    });
+});
+
+// User Sign Up
+authRouter.post('/user/signup', passport.authenticate('user-signup', { session: false }), async (req, res, next) => {
+    res.json({
+        message: 'User Signup successful',
         user: req.user
     });
 });
 
 
 
+// Login route for admins with valid credentials using passport
+authRouter.post('/admin/login', async (req, res, next) => {
+    passport.authenticate('admin-login', async (err, user, info) => {
+        try {
+            if (err || !user) {
+                const error = new Error('An error occurred.');
+                return next(error);
+            }
+            req.login(user, { session: false }, async (error) => {
+                if (error) return next(error);
+                const body = { _id: user._id, email: user.email };
+                const token = jwt.sign({ user: body }, process.env.JWT_SECRET);
+                return res.json({ token });
+            });
+        } catch (error) {
+            return next(error);
+        }
+    })(req, res, next);
+});
+
+
+
+
 // Login route for users with valid credentials using passport
-authRouter.post('/login', async (req, res, next) => {
-    passport.authenticate('login', async (err, user, info) => {
+authRouter.post('/user/login', async (req, res, next) => {
+    passport.authenticate('user-login', async (err, user, info) => {
         try {
             if (err || !user) {
                 const error = new Error('An error occurred.');
